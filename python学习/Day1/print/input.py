809025f539dea7d61759fd6a902d7047e558b7d9
Cyanvir <zzh80216@gmail.com>
2025-10-10 22:56:37 +0800

    增加注释

--- python学习/Day1/print/input.py
+++ python学习/Day1/print/input.py
@@ -1,3 +1,4 @@
-name=input("Enter your name:")
-print(f"你好，{name}！欢迎学习Python！")
-print(name)zz
+name=input("Enter your name:") #标准赋值语句，可以直接把要输出的写出来
+print(f"你好，{name}！欢迎学习Python！") #f 格式化字符串字面量
+print(name)
+print("你好，%s！欢迎学习Python！"% name)#不同的输出字面量的方法
