65aee23755f1639944c72b961c1df7d27df780cf
Cyanvir <zzh80216@gmail.com>
2025-10-11 17:22:47 +0800

    new

--- python学习/Day1/print/input.py
+++ python学习/Day1/print/input.py
@@ -1,4 +1,8 @@
 name=input("Enter your name:") #标准赋值语句，可以直接把要输出的写出来
-print(f"你好，{name}！欢迎学习Python！") #f 格式化字符串字面量
 print(name)
+print(f"你好，{name}！欢迎学习Python！") #f 格式化字符串字面量
 print("你好，%s！欢迎学习Python！"% name)#不同的输出字面量的方法
+print("你好，",name,"!欢迎学习Python！")
+print("你好，"+name+"!欢迎学习Python！")
+#逗号 , 分隔参数：print 自动在参数间加空格。
+#加号 + 拼接字符串：直接 “无缝” 连接字符串，无额外空格。
