8475ebf19d53454abd4b8bbf642454a0c8110291
Cyanvir <1264603383@qq.com>
2025-12-27 15:54:56 +0800

    day17

---
+++ c/day17/project/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "math_utils.h"
+int main() {
+    printf("5 + 3 = %d\n", add(5, 3));
+    printf("5 - 3 = %d\n", subtract(5, 3));
+    printf("5 * 3 = %d\n", multiply(5, 3));
+    printf("5 / 3 = %.2f\n", divide(5, 3));
+    return 0;
+}
---
+++ c/day17/project/math_utils.c
@@ -0,0 +1,14 @@
+#include "math_utils.h"
+int add(int a, int b) {
+    return a + b;
+}
+int subtract(int a, int b) {
+    return a - b;
+}
+int multiply(int a, int b) {
+    return a * b;
+}
+float divide(int a, int b) {
+    if(b != 0) return (float)a / b;
+    return 0;
+}
